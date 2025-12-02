0bf9955674364a0190836695353b4f4a21d26a81
codethub <111586060+codethub@users.noreply.github.com>
2025-12-02 03:47:08 +0000

    feat: add favicon generation and PWA support
    - Added a new SVG favicon file to the project.
    - Implemented a script to generate various favicon formats from the SVG.
    - Created TypeScript definitions for styled-components to define a color palette.
    - Updated TypeScript configuration files to include new scripts and definitions.
    - Configured Vite to support PWA with a manifest and icon settings.

---
+++ vite/README.md
@@ -0,0 +1,18 @@
+## [React-2048]
+
+A React implementation of [2048]built with [Typescript](https://www.typescriptlang.org/) and 💅 [styled-components](https://styled-components.com)
+
+### Demo
+
+Try online demo [here]
+
+### Getting Started
+
+```shell
+$ npm install
+$ npm start
+# open http://localhost:3000
+```
+
+### Liscense
+
---
+++ vite/vite.config.ts
@@ -0,0 +1,50 @@
+/* eslint-disable import/no-extraneous-dependencies */
+import { defineConfig } from 'vite';
+import type { PluginOption } from 'vite';
+import react from '@vitejs/plugin-react';
+import { VitePWA } from 'vite-plugin-pwa';
+import type { VitePWAOptions } from 'vite-plugin-pwa';
+/* eslint-enable import/no-extraneous-dependencies */
+
+const pwaOptions: Partial<VitePWAOptions> = {
+  registerType: 'autoUpdate',
+  manifest: {
+    id: '/2048/',
+    name: '2048',
+    short_name: '2048',
+    description: 'A React clone of 2048 game',
+    theme_color: '#ffffff',
+    icons: [
+      {
+        src: '/react-2048/assets/android-chrome-192x192.png',
+        sizes: '192x192',
+        type: 'image/png',
+      },
+      {
+        src: '/react-2048/assets/android-chrome-512x512.png',
+        sizes: '512x512',
+        type: 'image/png',
+      },
+    ],
+  },
+};
+
+export default defineConfig(({ mode }) => {
+  const isProd = mode === 'production';
+  const plugins: PluginOption[] = [react()];
+
+  if (isProd) {
+    plugins.push(VitePWA(pwaOptions));
+  }
+
+  return {
+    base: './',
+    server: {
+      port: 3000,
+    },
+    plugins,
+    build: {
+      emptyOutDir: true,
+    },
+  };
+});

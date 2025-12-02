d8658595d6af85c5b7b05984b31e6139bda20e34
codethub <111586060+codethub@users.noreply.github.com>
2025-12-02 04:24:02 +0000

    feat: wrap App component with WagmiProvider and QueryClientProvider for enhanced state management

--- vite/src/index.tsx
+++ vite/src/index.tsx
@@ -1,13 +1,23 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
+import { WagmiProvider } from 'wagmi';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import App from './App';
-import GlobalStyle from './components/GlobalStyle';
+import { config } from './web3/reown'; 
+import GlobalStyle from './components/GlobalStyle'; 
+
+const queryClient = new QueryClient();
 
 const container = document.getElementById('game') as HTMLElement;
 const root = createRoot(container);
+
 root.render(
-  <>
-    <GlobalStyle />
-    <App />
-  </>,
+  <React.StrictMode>
+    <GlobalStyle /> 
+    <WagmiProvider config={config}>
+      <QueryClientProvider client={queryClient}>
+        <App />
+      </QueryClientProvider>
+    </WagmiProvider>
+  </React.StrictMode>,
 );
